e62715f5c7d12dbc844d1a0de4675a5fc3bdc2ff
Тимур <timik44452@gmail.com>
2020-03-23 18:42:47 +0300

    added signin form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
+var signInOverlay;
+
+window.onmousedown = function(event){
+    if(event.target == this.signInOverlay){
+        this.closeSignIn();
+    }
+}
+
 function showSignIn(){
-    getTemplate('signin_form.html', () => {
-        document.body.innerHTML += getHTML();
-    });
+    if(signInOverlay){
+        signInOverlay.style.display = 'block';
+    }else{
+        getTemplate('signin_form.html', () => {
+            document.body.innerHTML += getHTML();
+            signInOverlay = document.getElementById('overlay');
+        });
+    }
+    
+}
+
+function closeSignIn(){
+    if(signInOverlay){
+        signInOverlay.style.display = 'none';
+    }
 }
